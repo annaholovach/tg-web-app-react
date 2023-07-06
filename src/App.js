e16c87ab961c3d4f216e61c2011d8d19a8ffba02
annaholovach <annaholovachanna@gmail.com>
2023-07-06 14:05:52 +0300

    modified form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './hooks/useTelegram'
 import Header from './components/header/header';
+import { Route, Routes } from 'react-router-dom';
+import ProductList from './components/productlist/productlist';
+import Form from './components/form/form';
 
 
 function App() {
@@ -14,7 +17,10 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <button onClick={onToggleButton}>toggle</button>
+      <Routes>
+        <Route index element = {<ProductList/>}/>
+        <Route path={'form'} element = {<Form/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/productlist/productlist.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './productlist.css'
+
+const ProductList = () => {
+    return (
+        <div>
+            productlist
+        </div>
+    )
+}
+
+export default ProductList
