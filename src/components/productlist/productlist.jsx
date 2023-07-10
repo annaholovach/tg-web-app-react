64e950a55a595d9946326a0eb68d96a4abe60e08
annaholovach <annaholovachanna@gmail.com>
2023-07-10 12:32:24 +0300

    first commit

--- src/components/productlist/productlist.jsx
+++ src/components/productlist/productlist.jsx
@@ -31,7 +31,7 @@ const ProductList = () => {
             totalPrice: getTotalPrice(addedItems),
             queryId
         }
-        fetch('http://localhost:8000/web-data', {
+        fetch('https://delirious-ambitious-peridot.glitch.me:8000/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
